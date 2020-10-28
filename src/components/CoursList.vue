50ad1ca535e21f9ab6b16d3609ed6b570bb6b8a6
Costet Rayane <rayane.costet@edu.devinci.fr>
2020-10-28 17:15:50 +0100

    add cours

---
+++ src/components/CoursList.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Liste des cours {{ coursData.name }} </h1>
+
+</template>
+
+<script>
+import coursData from '../assets/data/coues.json'
+import CoursCard from './CoursCard.vue'
+import SelectionBox from './SelectionBox.vue'
+export default {
+ 
+}
+</script>
+
+<style>
+</style>  
