ac852134f4479d394c3031bd65e849ea6ff9b5c9
ALI BENYAHIA Mehdi <mehdi.ali_benyahia@edu.devinci.fr>
2020-10-30 17:12:54 +0100

    push

--- src/components/global/BaseBouton.vue
+++ src/components/global/BaseBouton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="btn-blue">
+  <button class="bg-blue-500 text-white font-bold py-2 px-4 rounded">
     {{ text }}
   </button>
 </template>
@@ -12,7 +12,7 @@ export default {
 
 <style scoped>
 .btn-blue {
-  @apply bg-blue-500 text-white font-bold py-2 px-4 rounded;
+  @apply;
 }
 .btn-blue:hover {
   @apply bg-blue-700;
