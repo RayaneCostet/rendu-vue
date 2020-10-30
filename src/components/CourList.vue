2e2eb6935dcddd124b093f8f758c8790ccd41382
ALI BENYAHIA Mehdi <mehdi.ali_benyahia@edu.devinci.fr>
2020-10-30 17:57:28 +0100

    push

--- src/components/CourList.vue
+++ src/components/CourList.vue
@@ -1,56 +1,46 @@
 <template>
-
-  <img alt="Vue logo" src="../assets/img/logo.png">
+  <img alt="Vue logo" src="../assets/img/logo.jpg" />
   <div class="flex">
     <div class="w-1/5">
-      <selection-box
-        :selection="selection"
-      >
-      </selection-box>
+      <selection-box :selection="selection"> </selection-box>
     </div>
-    
 
     <div class="w-4/5 mt-2">
-      <cour-card 
+      <cour-card
         v-for="(cour, index) in team.cours"
         :key="index"
         :cour="cour"
         @add-cour="addCourToSelection"
-        >
+      >
       </cour-card>
     </div>
-    
-
   </div>
-    
 </template>
-  
+
 <script>
-import coursData from '../data/cours.json'
-import CourCard from './CourCard.vue'
-import SelectionBox from './SelectionBox.vue'
+import coursData from "../data/cours.json";
+import CourCard from "./CourCard.vue";
+import SelectionBox from "./SelectionBox.vue";
 
 export default {
   name: "cour-view",
   components: {
     CourCard,
-    SelectionBox
+    SelectionBox,
   },
   data() {
     return {
       team: coursData,
-      selection: []
-    }
+      selection: [],
+    };
   },
   methods: {
-    addCourToSelection(cour){
-      this.selection.push(cour)
-      this.$emit('child-event', cour)
-    }
-  }
-}
+    addCourToSelection(cour) {
+      this.selection.push(cour);
+      this.$emit("child-event", cour);
+    },
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
